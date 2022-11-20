c9d377cae039c6455cf7f24e0bdce5fde6566a6a
Aman Ibrahim <amanmibra@gmail.com>
2022-11-19 19:21:10 -0800

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from "react";
-import logo from "./images/logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import Login from "./pages/Login";
+
+// styles
 import "./styles/App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/catalog",
+    element: <Catalog />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
